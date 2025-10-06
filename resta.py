870d4a87a9cac571412d958ce94edc75cf99ff28
Sebassb <juansebastiansuarez2007@gmail.com>
2025-10-06 13:32:10 -0500

    Subo la funcion de resta

---
+++ resta.py
@@ -0,0 +1,8 @@
+def resta():
+    x = float(input("Ingrese el primer numero: "))
+    y = float(input("Ingrese el segundo numero a restar: "))
+
+    resultado = x - y
+    print(f"El resultado de la resta entre {x} y {y} es: {resultado} ")
+
+resta()
